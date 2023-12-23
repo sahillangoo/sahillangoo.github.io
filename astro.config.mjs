9859729aa5c22f3088815ded66b5a559641e0117
sahillangoo <40671195+sahillangoo@users.noreply.github.com>
2023-12-23 23:40:53 +0530

    ⌚ Update Astro configuration and reading time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import { remarkReadingTime } from './remark-reading-time.mjs';
+import { remarkReadingTime } from './remark-reading-time.mjs'
 import mdx from '@astrojs/mdx'
 import rehypeExternalLinks from 'rehype-external-links'
 import tailwind from '@astrojs/tailwind'
@@ -7,31 +7,18 @@ import sitemap from '@astrojs/sitemap'
 import compress from 'astro-compress'
 import lighthouse from 'astro-lighthouse'
 
+const repo = 'https://github.com/sahillangoo/sahillangoo.github.io'
+const site = 'https://sahillangoo.com'
+const author = 'Sahil Langoo'
+
 export default defineConfig({
-	repo: 'https://github.com/sahillangoo/sahillangoo.github.io',
-  site: 'https://sahillangoo.com',
-  author: 'Sahil Langoo',
-  prefetch: {
-    prefetchAll: true,
-  },
-  integrations: [
-    mdx({
-      optimize: true,
-    }),
-    tailwind({}),
-    sitemap(),
-    compress(),
-    lighthouse(),
-  ],
-  markdown: {
+	repo,
+	site,
+	author,
+	prefetch: { prefetchAll: true },
+	integrations: [mdx({ optimize: true }), tailwind(), sitemap(), compress(), lighthouse()],
+	markdown: {
 		remarkPlugins: [remarkReadingTime],
-		    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          content: { type: 'text', value: ' 🔗' }
-        }
-      ],
-    ]
-  },
+		rehypePlugins: [[rehypeExternalLinks, { content: { type: 'text', value: ' 🔗' } }]],
+	},
 })
