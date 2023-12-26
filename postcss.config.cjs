3e9d52bb9e50d9a7b332fcef79a63a878d27d2d7
sahillangoo <40671195+sahillangoo@users.noreply.github.com>
2023-12-27 00:21:44 +0530

    🌿Update Hero, postcss.config.cjs and MainLayout.astro

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {
-      config: path.join(__dirname, "tailwind.config.js"),
+    plugins: {
+        tailwindcss: {
+            config: path.join(__dirname, "tailwind.config.js"),
+        }
     },
-    autoprefixer: {
-      browsers: ["> 1%", "last 2 versions", "not ie <= 8"],
-    },
-  },
-};
+}
