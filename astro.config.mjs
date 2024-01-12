21d87c3c60c7330af406c4517fa2430fb7ebb16e
Sahil Langoo <40671195+sahillangoo@users.noreply.github.com>
2024-01-12 17:11:56 +0530

    ✨ Add new dependencies and update file paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,34 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-import rehypeExternalLinks from 'rehype-external-links'
-import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
-import { SITE } from '/src/consts.ts'
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+import { SITE } from '/src/consts.ts';
+import icon from "astro-icon";
+const repo = SITE.REPO;
+const site = SITE.URL;
+const author = SITE.AUTHOR;
 
-const repo = SITE.REPO
-const site = SITE.URL
-const author = SITE.AUTHOR
 // https://astro.build/config
 export default defineConfig({
-	repo,
-	site,
-	author,
-	integrations: [
-		mdx({
-			optimize: true,
-		}),
-		tailwind({
-			nesting: false, //  Enable nesting, like Sass
-      applyBaseStyles: false,
-    }),
-		sitemap(),
-	],
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					content: {
-						type: 'text',
-						value: ' 🔗',
-					},
-				},
-			],
-		],
-	},
-})
+  repo,
+  site,
+  author,
+  integrations: [mdx({
+    optimize: true
+  }), tailwind({
+    nesting: false,
+    //  Enable nesting, like Sass
+    applyBaseStyles: false
+  }), sitemap(), icon()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      }
+    }]]
+  }
+});
