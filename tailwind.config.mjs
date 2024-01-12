32bd5e21b9bead847b0e3eacf07192c5767690e4
Sahil Langoo <40671195+sahillangoo@users.noreply.github.com>
2024-01-12 20:38:39 +0530

    Update Tailwind config, home and remove unused SVG icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}"],
   theme: {
     extend: {
   backgroundImage: {
