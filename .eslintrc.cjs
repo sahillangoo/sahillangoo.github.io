5219425971a799017e91cd056bd1b40c4483b5e6
sahillangoo <40671195+sahillangoo@users.noreply.github.com>
2024-03-27 16:25:07 +0530

    🐛 Update global.css, MainNav.astro, .eslintrc.cjs, contact.astro, and tsconfig.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,7 @@ module.exports = {
     es2022: true,
     browser: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:astro/recommended',
-    // 'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:astro/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
